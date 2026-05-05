5e914e7b941a401ee4e877ce35f72d07ceb34d63
Kushal Gautam <167262630+KushGam@users.noreply.github.com>
2026-05-06 06:52:45 +1000

    Change Google Analytics script loading strategy

--- app/layout.tsx
+++ app/layout.tsx
@@ -56,10 +56,10 @@ export default function RootLayout({
       <body>
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-LT7YSXB2PP"
-          strategy="afterInteractive"
+          strategy="lazyOnload"
         />
 
-        <Script id="google-analytics" strategy="afterInteractive">
+        <Script id="google-analytics" strategy="lazyOnload">
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
